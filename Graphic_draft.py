f4cb04e13c3810468987983b2d313166c77842a9
Luhan Wang <luhan.wang@duke.edu>
2025-07-23 09:40:13 -0400

    update file name, attemp to work out gear position display, with problems

--- Graphic_draft.py
+++ Graphic_draft.py
@@ -3,6 +3,7 @@ import random
 import time
 MAX_RPM = 7000
 rpm = 0.0
+
 # notes: progress bar takes value from 0 to 1
 # need to normalize the rpm data so that it's within 0 to 1
 
@@ -19,14 +20,26 @@ def activate():
         print(updateRPM())
         dpg.set_value("RPM GAUGE", value=updateRPM())
         time.sleep(0.1)
-def deactivate():
-    dpg.destroy_viewport()
-    dpg.destroy_context()
 
 with dpg.window(label="RPM Tester"):
     dpg.add_progress_bar(tag = "RPM GAUGE", default_value=0, width=500, height=100)
     dpg.add_button(label="Start", tag="Start", callback=activate, width=100)
     # dpg.add_button(label="Stop", tag = "Stop", callback=deactivate, width=100)
+global gear
+gear = 0
+def upShift():
+    gear += 1
+    dpg.set_value("Gear Position", gear)
+
+def downShift():
+    if(gear > 0):
+        gear-=1
+        dpg.set_value("Gear Position", gear)
+
+with dpg.window(label = "Gear position"):
+    dpg.add_text(default_value='N', label="Gear Position", tag="Gear Position")
+    dpg.add_button(label = "Upshfit", tag = "Upshift", callback=upShift)
+    dpg.add_button(label="Downshift", tag="Downshift", callback=downShift)
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
