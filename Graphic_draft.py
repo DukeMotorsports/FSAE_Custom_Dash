960cfb74b7c51efadb2767016862caa1006b543b
Luhan Wang <luhan.wang@duke.edu>
2025-07-28 17:09:13 -0400

    fixed gear button bug

--- Graphic_draft.py
+++ Graphic_draft.py
@@ -22,20 +22,22 @@ def activate():
         time.sleep(0.1)
 
 def upShift():
+    global gear
     gear += 1
     dpg.set_value("Gear Position", value = gear)
 
 def downShift():
+    global gear
     if(gear > 0):
         gear-=1
         dpg.set_value("Gear Position", value = gear)
 
-with dpg.window(label="RPM Tester"):
+with dpg.window(label="RPM Tester", pos=(0,0), width=100, height=10):
     dpg.add_progress_bar(tag = "RPM GAUGE", default_value=0, width=500, height=100)
     dpg.add_button(label="Start", tag="Start", callback=activate, width=100)
     # dpg.add_button(label="Stop", tag = "Stop", callback=deactivate, width=100)
 
-with dpg.window(label = "Gear position"):
+with dpg.window(label = "Gear position", pos=(100,200)):
     dpg.add_text(default_value='N', label="Gear Position", tag="Gear Position")
     dpg.add_button(label = "Upshfit", tag = "Upshift", callback=upShift)
     dpg.add_button(label="Downshift", tag="Downshift", callback=downShift)
