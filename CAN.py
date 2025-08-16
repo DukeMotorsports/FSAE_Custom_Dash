f74a3f4586e33d07c9dbfc6681f9c8945fbf6a42
Luhan Wang <luhan.wang@duke.edu>
2025-08-16 13:21:43 -0400

    added test code to usb to CAN reader

--- CAN.py
+++ CAN.py
@@ -1,20 +1,10 @@
 import can
 
-# Candlelight firmware on Linux
-#bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=500000)
+# Replace "COM3" with your actual COM port
+bus = can.interface.Bus(interface="slcan", channel="COM3", bitrate=500000)
 
-# Stock slcan firmware on Linux
-bus = can.interface.Bus(bustype='slcan', channel='/dev/ttyACM0', bitrate=500000)
-
-# Stock slcan firmware on Windows
-bus = can.interface.Bus(bustype='slcan', channel='COM0', bitrate=500000)
-
-msg = can.Message(arbitration_id=0xc0ffee,
-                  data=[0, 25, 0, 1, 3, 1, 4, 1],
-                  is_extended_id=True)
-
-try:
-    bus.send(msg)
-    print("Message sent on {}".format(bus.channel_info))
-except can.CanError:
-    print("Message NOT sent")
+print("Listening for CAN messages...")
+while True:
+    msg = bus.recv()
+    if msg:
+        print(f"ID: {msg.arbitration_id:#04x}  Data: {msg.data.hex()}")
